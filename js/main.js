e367abdd69e2829654555de6298b32d7eb0c10a9
Andrea Elena Febres Medina <andreae.febresm@gmail.com>
2021-10-12 11:45:50 +0200

    factsr

--- js/main.js
+++ js/main.js
@@ -1,74 +1,206 @@
 $(document).ready(function() {
 
-
-//--PARALLAX 
+  //--PARALLAX
   var currentX = '';
   var currentY = '';
   var movementConstant = .015;
   $(document).mousemove(function(e) {
-    if(currentX == '') currentX = e.pageX;
+    if (currentX == '') currentX = e.pageX;
     var xdiff = e.pageX - currentX;
     currentX = e.pageX;
-     if(currentY == '') currentY = e.pageY;
+    if (currentY == '') currentY = e.pageY;
     var ydiff = e.pageY - currentY;
     currentY = e.pageY;
     $('.immagini').each(function(i, el) {
-        var movement = (i + 2) * (xdiff * movementConstant);
-  	  var movementy = (i + 2) * (ydiff * movementConstant);
-        var newX = $(el).position().left + movement;
-  	  var newY = $(el).position().top + movementy;
-        $(el).css('left', newX + 'px');
-  	  $(el).css('top', newY + 'px');
+      var movement = (i + 2) * (xdiff * movementConstant);
+      var movementy = (i + 2) * (ydiff * movementConstant);
+      var newX = $(el).position().left + movement;
+      var newY = $(el).position().top + movementy;
+      $(el).css('left', newX + 'px');
+      $(el).css('top', newY + 'px');
     });
   });
 
-
-//--BARRA TEMPO
+  //--BARRA TEMPO
   var tempo = gsap.timeline();
   //--Trasformazione della barra
-  tempo.to(".rettangolo-rosso", {duration: 30, delay: 5, scaleY: 0, transformOrigin: "bottom", backgroundColor: "#FF0000"});
+  tempo.to(".rettangolo-rosso", {
+    duration: 30,
+    delay: 5,
+    scaleY: 0,
+    transformOrigin: "bottom",
+    backgroundColor: "#FF0000"
+  });
   // --Lampeggio da 15sec in poi
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 15, opacity: 0}, "<"); //1
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //2
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //3
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //4
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //5
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //6
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //7
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //8
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //9
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //10
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //11
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //12
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //13
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //14
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //15
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //16
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //17
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //18
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //19
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //20
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //21
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //22
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //23
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //24
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //25
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //26
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //27
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //28
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //29
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //30
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 0}, "<"); //31
-  tempo.to(".rettangolo-rosso", {duration: 0.5, delay: 0.5, opacity: 1}, "<"); //32
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 15,
+    opacity: 0
+  }, "<"); //1
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<"); //2
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<"); //3
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<"); //4
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<"); //5
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<"); //6
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<"); //7
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<"); //8
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<"); //9
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<"); //10
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<"); //11
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<"); //12
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<"); //13
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<"); //14
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<"); //15
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<"); //16
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<"); //17
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<"); //18
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<"); //19
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<"); //20
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<"); //21
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<"); //22
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<"); //23
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<"); //24
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<"); //25
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<"); //26
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<"); //27
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<"); //28
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<"); //29
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<"); //30
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<"); //31
+  tempo.to(".rettangolo-rosso", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<"); //32
 
 
-//--TRANSIZIONE
-  window.addEventListener("beforeunload", function () {
+  //--TRANSIZIONE
+  window.addEventListener("beforeunload", function() {
     document.body.classList.add("animate-out");
   });
 
 
-//--CLICK FUNCTION
+  //--CLICK FUNCTION
   var giusto = 1;
   var sbagliato = 0;
 
@@ -92,12 +224,36 @@ $(document).ready(function() {
     window.open('../right-wrong/wrong2.html?q2=' + sbagliato2, '_self');
   });
   var tempo = gsap.timeline(); //--Rettangolo che pulsa nel fact
-  tempo.to("#ZOOMhint2", {duration: 0.5, delay: 0.5, opacity: 0});
-  tempo.to("#ZOOMhint2", {duration: 0.5, delay: 0.5, opacity: 1}, "<");
-  tempo.to("#ZOOMhint2", {duration: 0.5, delay: 0.5, opacity: 0}, "<");
-  tempo.to("#ZOOMhint2", {duration: 0.5, delay: 0.5, opacity: 1}, "<");
-  tempo.to("#ZOOMhint2", {duration: 0.5, delay: 0.5, opacity: 0}, "<");
-  tempo.to("#ZOOMhint2", {duration: 0.5, delay: 0.5, opacity: 1}, "<");
+  tempo.to("#ZOOMhint2", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  });
+  tempo.to("#ZOOMhint2", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<");
+  tempo.to("#ZOOMhint2", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<");
+  tempo.to("#ZOOMhint2", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<");
+  tempo.to("#ZOOMhint2", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<");
+  tempo.to("#ZOOMhint2", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<");
 
   //--Question 3
   $('#RIGHT03').click(function() {
@@ -125,12 +281,36 @@ $(document).ready(function() {
     window.open('../right-wrong/wrong4.html?q4=' + sbagliato4, '_self');
   });
   var tempo = gsap.timeline(); //--Rettangolo che pulsa nel fact
-  tempo.to("#ZOOMhint4", {duration: 0.5, delay: 0.5, opacity: 0});
-  tempo.to("#ZOOMhint4", {duration: 0.5, delay: 0.5, opacity: 1}, "<");
-  tempo.to("#ZOOMhint4", {duration: 0.5, delay: 0.5, opacity: 0}, "<");
-  tempo.to("#ZOOMhint4", {duration: 0.5, delay: 0.5, opacity: 1}, "<");
-  tempo.to("#ZOOMhint4", {duration: 0.5, delay: 0.5, opacity: 0}, "<");
-  tempo.to("#ZOOMhint4", {duration: 0.5, delay: 0.5, opacity: 1}, "<");
+  tempo.to("#ZOOMhint4", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  });
+  tempo.to("#ZOOMhint4", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<");
+  tempo.to("#ZOOMhint4", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<");
+  tempo.to("#ZOOMhint4", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<");
+  tempo.to("#ZOOMhint4", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<");
+  tempo.to("#ZOOMhint4", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<");
 
   //--Question 5
   $('#RIGHT05').click(function() {
@@ -195,12 +375,36 @@ $(document).ready(function() {
     window.open('../right-wrong/wrong6.html?q6=' + sbagliato6, '_self');
   });
   var tempo = gsap.timeline(); //--Rettangolo che pulsa nel fact
-  tempo.to("#ZOOMhint6", {duration: 0.5, delay: 0.5, opacity: 0});
-  tempo.to("#ZOOMhint6", {duration: 0.5, delay: 0.5, opacity: 1}, "<");
-  tempo.to("#ZOOMhint6", {duration: 0.5, delay: 0.5, opacity: 0}, "<");
-  tempo.to("#ZOOMhint6", {duration: 0.5, delay: 0.5, opacity: 1}, "<");
-  tempo.to("#ZOOMhint6", {duration: 0.5, delay: 0.5, opacity: 0}, "<");
-  tempo.to("#ZOOMhint6", {duration: 0.5, delay: 0.5, opacity: 1}, "<");
+  tempo.to("#ZOOMhint6", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  });
+  tempo.to("#ZOOMhint6", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<");
+  tempo.to("#ZOOMhint6", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<");
+  tempo.to("#ZOOMhint6", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<");
+  tempo.to("#ZOOMhint6", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<");
+  tempo.to("#ZOOMhint6", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<");
 
   //--Question 7
   $('#RIGHT07').click(function() {
@@ -298,16 +502,40 @@ $(document).ready(function() {
     window.open('../right-wrong/wrong12.html?q12=' + sbagliato12, '_self');
   });
   var tempo = gsap.timeline(); //--Rettangolo che pulsa nel fact
-  tempo.to("#ZOOMhint12", {duration: 0.5, delay: 0.5, opacity: 0});
-  tempo.to("#ZOOMhint12", {duration: 0.5, delay: 0.5, opacity: 1}, "<");
-  tempo.to("#ZOOMhint12", {duration: 0.5, delay: 0.5, opacity: 0}, "<");
-  tempo.to("#ZOOMhint12", {duration: 0.5, delay: 0.5, opacity: 1}, "<");
-  tempo.to("#ZOOMhint12", {duration: 0.5, delay: 0.5, opacity: 0}, "<");
-  tempo.to("#ZOOMhint12", {duration: 0.5, delay: 0.5, opacity: 1}, "<");
+  tempo.to("#ZOOMhint12", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  });
+  tempo.to("#ZOOMhint12", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<");
+  tempo.to("#ZOOMhint12", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<");
+  tempo.to("#ZOOMhint12", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<");
+  tempo.to("#ZOOMhint12", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+  }, "<");
+  tempo.to("#ZOOMhint12", {
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 1
+  }, "<");
 
   //--Animazione
   $('#testuale').hide();
-  setTimeout(function(){
+  setTimeout(function() {
     $('#testuale').fadeIn();
   }, 3000);
 
